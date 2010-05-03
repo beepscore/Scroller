4e87b0d249e3015a7de8e6de775f6089bc29d473
beepscore <support@beepscore.com>
2010-05-03 13:44:10 -0700

    Initial commit.  Deleted ScrollViewImageGalleryViewController before starting repository

---
+++ Classes/ScrollViewImageGalleryAppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  Created by Björn Sållarp on 2009-06-14.
+//  NO Copyright 2009 MightyLittle Industries. NO rights reserved.
+// 
+//  Use this code any way you like. If you do like it, please
+//  link to my blog and/or write a friendly comment. Thank you!
+//
+//  Read my blog @ http://blog.sallarp.com
+//
+#import <UIKit/UIKit.h>
+
+@class ImageGallery;
+
+@interface ScrollViewImageGalleryAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+    ImageGallery *viewController;
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet ImageGallery *viewController;
+
+@end
+
